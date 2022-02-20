6d8969a27579799b83db6757d755e6d816cd7578
Mayconn Coelho <mayconn@netpark.com.br>
2022-02-20 20:00:26 -0300

    Added ChakraUi Provider

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from "react-redux";
-import getStore from "./store/getStore";
+import { Provider as ReduxProvider } from 'react-redux';
+import getStore from './store/getStore';
 import reducers from './reducers';
 import Main from './Main';
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
 import { ToastContainer } from 'react-toastify';
 
-
 // import main sass file
-import "./sass/app.scss";
-import "react-toastify/dist/ReactToastify.min.css";
+import './sass/app.scss';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <ReduxProvider store={getStore(reducers)}>
       <BrowserRouter>
-        <Main />
+        <ChakraProvider>
+          <Main />
+        </ChakraProvider>
       </BrowserRouter>
       <ToastContainer />
     </ReduxProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
